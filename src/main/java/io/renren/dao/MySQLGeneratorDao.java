7364ac9fe3e952cd32944d5446777e7638f80e1d
布乃同 <abuwyyx@163.com>
2023-08-11 11:46:55 +0800

    first commit

---
+++ src/main/java/io/renren/dao/MySQLGeneratorDao.java
@@ -0,0 +1,11 @@
+package io.renren.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+
+@Mapper
+public interface MySQLGeneratorDao extends GeneratorDao {
+	
+
+}
---
+++ src/main/java/io/renren/dao/PostgreSQLGeneratorDao.java
@@ -0,0 +1,11 @@
+package io.renren.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * PostgreSQL代码生成器
+ */
+@Mapper
+public interface PostgreSQLGeneratorDao extends GeneratorDao {
+
+}
